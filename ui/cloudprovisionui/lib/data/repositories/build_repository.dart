c5b7978a6a8e63590de6a20d38b889ec3f59080c
Andrey Shakirov <andreyshakirov@google.com>
2022-09-15 14:12:53 -0400

    Added build endpoint

--- ui/cloudprovisionui/lib/data/repositories/build_repository.dart
+++ ui/cloudprovisionui/lib/data/repositories/build_repository.dart
@@ -11,11 +11,8 @@ import 'package:http/http.dart' as http;
 const cloudRunUrl = "cloud-provision-server-4mizdq5szq-ue.a.run.app";
 
 class BuildRepository {
-  Future<String> deployTemplate(TemplateModel template) async {
-    return await callCloudProvisionService(template);
-  }
-
-  Future<String> callCloudProvisionService(TemplateModel template) async {
+  Future<String> deployTemplate(String projectId, TemplateModel template,
+      Map<String, dynamic> formFieldValuesMap) async {
     final user = FirebaseAuth.instance.currentUser!;
     var identityToken = await user.getIdToken();
     String result = "";
@@ -30,7 +27,71 @@ class BuildRepository {
 
       var url = Uri.https(cloudRunUrl, endpointPath);
 
-      var body = json.encode({"template_id": "${template.id}"});
+      var body = json.encode({
+        "project_id": projectId,
+        "template_id": "${template.id}",
+        "params": formFieldValuesMap
+      });
+
+      var response = await http.post(url, headers: requestHeaders, body: body);
+
+      result = response.body;
+    } catch (e) {
+      print(e);
+    }
+
+    return result;
+  }
+
+  Future<String> getBuildDetails(String projectId, String buildId) async {
+    final user = FirebaseAuth.instance.currentUser!;
+    var identityToken = await user.getIdToken();
+    String result = "";
+
+    try {
+      Map<String, String> requestHeaders = {
+        HttpHeaders.authorizationHeader: "Bearer " + identityToken,
+        HttpHeaders.contentTypeHeader: "application/json"
+      };
+
+      var endpointPath = '/v1/builds';
+      // could be passed as request params
+      final queryParameters = {
+        'projectId': projectId,
+        'buildId': buildId,
+      };
+
+      var url = Uri.https(cloudRunUrl, endpointPath, queryParameters);
+
+      var response = await http.get(url, headers: requestHeaders);
+
+      result = response.body;
+    } catch (e) {
+      print(e);
+    }
+
+    return result;
+  }
+
+  Future<String> runTrigger(String projectId, String appName) async {
+    final user = FirebaseAuth.instance.currentUser!;
+    var identityToken = await user.getIdToken();
+    String result = "";
+
+    try {
+      Map<String, String> requestHeaders = {
+        HttpHeaders.authorizationHeader: "Bearer " + identityToken,
+        HttpHeaders.contentTypeHeader: "application/json"
+      };
+
+      var endpointPath = '/v1/triggers';
+
+      var url = Uri.https(cloudRunUrl, endpointPath);
+
+      var body = json.encode({
+        "project_id": projectId,
+        "app_name": appName,
+      });
 
       var response = await http.post(url, headers: requestHeaders, body: body);
 
