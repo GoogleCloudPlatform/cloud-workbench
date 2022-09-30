a10f79daa87a6c118775d81b5e21630054ea9b38
Andrey Shakirov <andreyshakirov@google.com>
2022-09-30 14:09:11 -0400

    added service model

---
+++ ui/cloudprovisionui/lib/repository/models/service.dart
@@ -0,0 +1,40 @@
+class Service {
+  String name;
+  String owner;
+  String templateName;
+  String instanceRepo;
+  String region;
+  String projectId;
+  DateTime deploymentDate;
+
+  Service(
+    this.name,
+    this.owner,
+    this.templateName,
+    this.instanceRepo,
+    this.region,
+    this.projectId,
+    this.deploymentDate,
+  );
+
+  Service.fromJson(Map<String, dynamic> parsedJson)
+      : name = parsedJson['name'],
+        owner = parsedJson['owner'],
+        templateName = parsedJson['templateName'],
+        instanceRepo = parsedJson['instanceRepo'],
+        region = parsedJson['region'],
+        projectId = parsedJson['projectId'],
+        deploymentDate = DateTime.parse(parsedJson['deploymentDate']);
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'owner': owner,
+      'templateName': templateName,
+      'instanceRepo': instanceRepo,
+      'region': region,
+      'projectId': projectId,
+      'deploymentDate': deploymentDate,
+    };
+  }
+}
