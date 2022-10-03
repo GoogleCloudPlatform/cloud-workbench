fdfaf53ef65651ac238e5278bd0e5f53ef608d17
Andrey Shakirov <andreyshakirov@google.com>
2022-10-03 11:18:20 -0400

    Added new param

--- cloud-run/lib/models/param_model.dart
+++ cloud-run/lib/models/param_model.dart
@@ -1,19 +1,20 @@
-class ParamModel {
+class Param {
   String param;
   String label;
   String description;
   String type;
   bool required;
+  bool? display;
 
-  ParamModel(
-      this.param, this.label, this.description, this.type, this.required);
+  Param(this.param, this.label, this.description, this.type, this.required);
 
-  ParamModel.fromJson(Map<String, dynamic> parsedJson)
+  Param.fromJson(Map<String, dynamic> parsedJson)
       : param = parsedJson['param'],
         label = parsedJson['label'],
         description = parsedJson['description'],
         type = parsedJson['type'],
-        required = parsedJson['required'];
+        required = parsedJson['required'],
+        display = parsedJson['display'] == null ? true : parsedJson['display'];
 
   Map<String, dynamic> toJson() {
     return {
@@ -22,6 +23,7 @@ class ParamModel {
       'description': description,
       'type': type,
       'required': required,
+      'display': display == null ? true : display,
     };
   }
 }
