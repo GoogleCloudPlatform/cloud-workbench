5226690bb6e5815f877705b39fd609a7c2de206b
Andrey Shakirov <andreyshakirov@google.com>
2022-10-24 22:23:12 -0400

    switched to main branch

--- cloud-run/lib/services/TemplatesService.dart
+++ cloud-run/lib/services/TemplatesService.dart
@@ -10,7 +10,7 @@ class TemplatesService {
 
   // TODO: Read/inject URI from configuration
   String templatesUri =
-      "https://raw.githubusercontent.com/gitrey/cp-templates/feature/new-app-metadata/templates.json";
+      "https://raw.githubusercontent.com/gitrey/cp-templates/main/templates.json";
 
   String communityCatalogUrl =
       "https://raw.githubusercontent.com/gitrey/community-templates/main/templates.json";
