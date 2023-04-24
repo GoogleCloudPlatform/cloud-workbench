bb8345a69c4c47911fd280bf9775f82d2feee83a
Andrey Shakirov <andreyshakirov@google.com>
2023-04-24 09:50:17 -0400

    Switched to env variable for API

--- ui/cloudprovisionui/lib/repository/service/base_service.dart
+++ ui/cloudprovisionui/lib/repository/service/base_service.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class BaseService {
-  final String cloudProvisionServerUrl = dotenv.get('CLOUD_PROVISION_API_URL');
+  static const String cloudProvisionServerUrl = String.fromEnvironment('CLOUD_PROVISION_API_URL', defaultValue: "");
 
   Uri getUrl(String endpointPath, {Map<String, dynamic>? queryParameters}) {
     var url = Uri.https(cloudProvisionServerUrl, endpointPath, queryParameters);
