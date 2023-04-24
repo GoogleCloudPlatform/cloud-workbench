8930406724529ea38fa4463c358e7e85302007d4
Andrey Shakirov <andreyshakirov@google.com>
2023-04-24 09:51:29 -0400

    Added support to run locally

--- cloud-run/lib/server.dart
+++ cloud-run/lib/server.dart
@@ -5,6 +5,7 @@ import 'package:cloud_provision_server/middleware/CORSHeadersHandler.dart';
 import 'package:cloud_provision_server/middleware/TokenValidationHandler.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_static/shelf_static.dart';
 
 Future<void> main() async {
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
@@ -13,19 +14,41 @@ Future<void> main() async {
   TokenValidationHandler tokenValidationHandler = TokenValidationHandler();
   CORSHeadersHandler corsHeadersHandler = CORSHeadersHandler();
 
-  final handler = const Pipeline()
+  final handlerPipeline = const Pipeline()
       .addMiddleware(corsHeadersHandler.corsHeadersHandler())
       .addMiddleware(tokenValidationHandler.tokenValidationHandler())
       .addHandler(serverController.handler);
 
+  var handler;
+
+  if (Platform.isMacOS || Platform.isWindows) {
+    handler = const Pipeline()
+        .addMiddleware(corsHeadersHandler.corsHeadersHandler())
+        .addMiddleware(tokenValidationHandler.tokenValidationHandler())
+        .addHandler(serverController.handler);
+  } else {
+    var flutterWebAppPath =
+        Platform.environment['WEBAPP_PATH'] ?? "/app/bin/public";
+
+    final staticHandler =
+        createStaticHandler(flutterWebAppPath, defaultDocument: 'index.html');
+    final staticAssetsHandler =
+        createStaticHandler(flutterWebAppPath + '/assets');
+
+    handler = Cascade()
+        .add(staticHandler)
+        .add(staticAssetsHandler)
+        .add(handlerPipeline)
+        .handler;
+  }
+
   final server = await shelf_io.serve(
     handler,
-    InternetAddress.anyIPv4, // Allows external connections
+    InternetAddress.anyIPv4,
     port,
   );
 
-  // Enable content compression
   server.autoCompress = true;
 
-  print('Serving at http://${server.address.host}:${server.port}');
+  print('Serving at ${server.address.host}:${server.port}');
 }
